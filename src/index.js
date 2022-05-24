e612e0d13e5356ca18a30218de0ae97519ff26e9
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-23 23:28:39 -0700

    update logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 function insertionSort(arr) {
-  // insert current element in the right place (for loop)
-  // while loop to push everything right like in a linkedlist
-  // return arr
+  // insert current element in the right place (for loop), start from 2nd element
+  for (let i = 1; i < arr.length; i++) {
+    let temp = arr[i];
+    let tempIdx = i;
+    let j = i - 1;
+    // the element we're replacing is less than the element in front of it
+    while (j >= 0 && temp < arr[j]) {
+      // push everything right like in a linkedlist
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    // now i need to insert after pushing things back
+    arr[j + 1] = temp;
+    j = j - 1;
+  }
+  return arr;
 }
